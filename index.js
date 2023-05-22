73d437ba6cab46328a08fe0a9778bdb217cfb37f
Matt Gaughan <mattgaugs@gmail.com>
2023-05-22 14:15:31 -0400

    API integration with Mongoose and MDB- connecting to Node Site

--- index.js
+++ index.js
@@ -7,6 +7,19 @@
 //update user info
 // What information could your client want to remove (DELETE)?
 //delete list from their account, deactivate their account
+//npm install mongoose
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect("mongodb://127.0.0.1:27017/movieDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const bodyParser = require("body-parser");
 
 const express = require("express"),
   morgan = require("morgan"), //allows for non-fs logging
@@ -18,6 +31,8 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 });
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let topMovies = [
   {
@@ -73,16 +88,48 @@ app.get("/secreturl", (req, res) => {
   res.send("This is a secret url with super top-secret content.");
 });
 
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 app.get("/movies", (req, res) => {
-  res.json(topMovies);
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.get("/movies/:movieTitle", (req, res) => {
-  res.json(
-    topMovies.find((movie) => {
-      return movie.name === req.params.name;
+  Movies.findOne({ Title: req.params.movieTitle })
+    .then((movie) => {
+      res.json(movie);
     })
-  );
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.get("/genres", (req, res) => {
@@ -103,14 +150,57 @@ app.get("/directors/:name", (req, res) => {
   );
 });
 
-app.post("/users/:name", (req, res) => {
-  res.send(
-    "Successful POST request for creating/entering data on a new user (registered)"
-  );
+app.post("/users", (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
-app.put("/user/userInfo/:name", (req, res) => {
-  res.send("Successful PUT request for updating user info (name, birthday)");
+app.put("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username }).then((user) => {
+    if (user) {
+      Users.updateOne(
+        { Username: req.params.Username },
+        {
+          $set: {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+          },
+        }
+      )
+        .then((user) => {
+          res.status(201).json(user);
+        })
+        .catch((error) => {
+          console.error(error);
+          return res.status(500).send("Error: " + error);
+        });
+    } else {
+      return res.status(400).send(req.params.Username + " does not exist");
+    }
+  });
 });
 
 app.post("/user/lists/:name", (req, res) => {
@@ -123,8 +213,19 @@ app.put("/user/lists/:name", (req, res) => {
 app.delete("/user/lists/:name", (req, res) => {
   res.send("Successful DELETE request for updating list info (remove movie)");
 });
-app.delete("/users/:name", (req, res) => {
-  res.send("Successful DELETE request for deleting user account");
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.use("/", express.static("public")); //allow access to multiple files like documentation
