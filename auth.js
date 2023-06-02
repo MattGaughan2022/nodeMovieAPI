2405659b1b08ebc57916d42db1c5215f119df9fd
Matt Gaughan <mattgaugs@gmail.com>
2023-06-02 13:30:06 -0400

    push 3

--- auth.js
+++ auth.js
@@ -36,7 +36,7 @@ module.exports = (router) => {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
+        return res.status(200).json({ user, token });
       });
     })(req, res);
   });
