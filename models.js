491e741f306f08faf6a26f4797f5eac70915a992
Matt Gaughan <mattgaugs@gmail.com>
2023-06-02 15:16:17 -0400

    patch .6 fix 2.10

--- models.js
+++ models.js
@@ -25,6 +25,18 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   //FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
+
+let genreSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Description: { type: String, required: true },
+});
+
+let directorSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Bio: { type: String, required: true },
+  Birthyear: { type: Number, required: true },
+});
+
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
@@ -35,6 +47,10 @@ userSchema.methods.validatePassword = function (password) {
 
 let Movie = mongoose.model("Movie", movieSchema);
 let User = mongoose.model("User", userSchema);
+let Genre = mongoose.model("Genre", genreSchema);
+let Director = mongoose.model("Director", directorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
