946908f84073ac4c7e288a3a33f46f447af764c4
Matt Gaughan <mattgaugs@gmail.com>
2023-06-15 16:54:36 -0400

    potential fix or error report

--- auth.js
+++ auth.js
@@ -21,7 +21,7 @@ module.exports = (router) => {
       if (error || !user) {
         console.log(error);
         return res.status(400).json({
-          message: "Something is not working.",
+          message: "Something is not working." + error,
           user: user,
         });
       }
