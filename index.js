09ee9e4e96d36960bb544f01a0860d59882b5f6a
Matt Gaughan <mattgaugs@gmail.com>
2023-06-27 11:43:20 -0400

    push /update test

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// What information could the client want to retrieve (or GET) from the server?
-//list of movies, their directors, genres, actors?
-// What information could it want to add (or POST)?
-//add a new list to their account, add a movie to their list
-// What information could your client want to update (PUT)?
-//remove a movie from their list, change favorite genre on their account
-//update user info
-// What information could your client want to remove (DELETE)?
-//delete list from their account, deactivate their account
-//npm install mongoose
 const mongoose = require("mongoose");
 const Models = require("./models.js");
 const { check, validationResult } = require("express-validator");
@@ -15,11 +5,6 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
-
-// mongoose.connect("mongodb://127.0.0.1:27017/movieDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
@@ -41,8 +26,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(express.bodyParser());
-
 const cors = require("cors");
 let allowedOrigins = [
   "http://localhost:8080",
@@ -51,12 +34,10 @@ let allowedOrigins = [
 ];
 
 app.use(
-  //setting specific sites to be permitted access to requests. ^v
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
         let message =
           "The CORS policy for this application doesn’t allow access from origin " +
           origin;
@@ -72,7 +53,6 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
-// setup the logger
 app.use(morgan("common", { stream: accessLogStream }));
 
 app.get("/", (req, res) => {
@@ -114,22 +94,18 @@ app.get(
   }
 );
 
-app.get(
-  "/movies",
-  // passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .populate("Genre", "Name -_id")
-      .populate("Director", "Name -_id")
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .populate("Genre", "Name -_id")
+    .populate("Director", "Name -_id")
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 app.get(
   "/movies/:movieTitle",
@@ -208,11 +184,6 @@ app.get(
 
 app.post(
   "/users",
-  // Validation logic here for request
-  //you can either use a chain of methods like .not().isEmpty()
-  //which means "opposite of isEmpty" in plain english "is not empty"
-  //or use .isLength({min: 5}) which means
-  //minimum value of 5 characters are only allowed
   [
     check("Username", "Username is required").isLength({ min: 5 }),
     check(
@@ -223,7 +194,6 @@ app.post(
     check("Email", "Email does not appear to be valid").isEmail(),
   ],
   (req, res) => {
-    // check the validation object for errors
     let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -256,6 +226,68 @@ app.post(
       });
   }
 );
+
+app
+  .put(
+    //register copypasta
+    "/users/:Username",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      [
+        check("Username", "Username is required").isLength({ min: 5 }),
+        check(
+          "Username",
+          "Username contains non alphanumeric characters - not allowed."
+        ).isAlphanumeric(),
+        check(
+          "OldPassword",
+          "Current password must be entered to make changes."
+        )
+          .not()
+          .isEmpty(),
+        check("Email", "Email does not appear to be valid").isEmail(),
+      ],
+        // alert("Current password must be entered to make changes.")
+        (req, res) => {
+          let errors = validationResult(req);
+
+          if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+          }
+          if (
+            req.body.OldUsername !== req.body.Username &&
+            Users.findOne({ Username: req.body.Username })
+          ) {
+            res.status(400).send(req.body.Username + "already exists");
+          }
+          if (req.body.OldUsername === req.params.Username) {
+            let hashedPassword = Users.hashPassword(req.body.Password);
+            Users.findOneAndUpdate(
+              { Username: req.body.OldUsername },
+              {
+                $set: {
+                  Username: req.body.Username,
+                  Password: hashedPassword,
+                  Email: req.body.Email,
+                  Birthday: req.body.Birthday,
+                },
+              }
+            )
+              .then((user) => {
+                res.status(201).json(user);
+              })
+              .catch((error) => {
+                console.error(error);
+                res.status(500).send("Error: " + error);
+              });
+          } else req.status(401).send(req.body.OldUsername + "unauthorized.");
+        };
+    }
+  )
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send("Error: " + error);
+  });
 
 app.put(
   "/users/:Username",
