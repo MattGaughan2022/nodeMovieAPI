f0cee71130b516fe94586bdb796abc7e1fb23c98
Matt Gaughan <mattgaugs@gmail.com>
2023-07-06 20:08:09 -0400

    delete user implement 1

--- index.js
+++ index.js
@@ -363,34 +363,39 @@ passport.authenticate("jwt", { session: false }),
 
 );
 
-app.put("/users/:Username/lists/:ListName/:MovieID", (req, res) => {
-  res.send(
-    "Successful PUT request for updating list info (add movie) (Not Yet Implemented)"
-  );
-});
-app.delete("/users/:Username/lists/:ListName/:MovieID", (req, res) => {
-  res.send(
-    "Successful DELETE request for updating list info (remove movie) (Not Yet Implemented)"
-  );
-});
-// app.delete(
-//   "/users/:Username",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     Users.findOneAndRemove({ Username: req.params.Username })
-//       .then((user) => {
-//         if (!user) {
-//           res.status(400).send(req.params.Username + " was not found");
-//         } else {
-//           res.status(200).send(req.params.Username + " was deleted.");
-//         }
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//         res.status(500).send("Error: " + err);
-//       });
-//   }
-// );
+// app.put("/users/:Username/lists/:ListName/:MovieID", (req, res) => {
+//   res.send(
+//     "Successful PUT request for updating list info (add movie) (Not Yet Implemented)"
+//   );
+// });
+// app.delete("/users/:Username/lists/:ListName/:MovieID", (req, res) => {
+//   res.send(
+//     "Successful DELETE request for updating list info (remove movie) (Not Yet Implemented)"
+//   );
+// });
+app.delete(
+  "/users/:Username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if(req.params.Username === req.user.Username){
+    Users.findOneAndRemove({ Username: req.params.Username })
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + " was not found");
+        } else {
+          res.status(200).send(req.params.Username + " was deleted.");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+    }
+    else{
+      res.status(500).send("Unauthorized or fatal error.");
+    }
+  }
+);
 
 app.use("/", express.static("public")); //allow access to multiple files like documentation
 
